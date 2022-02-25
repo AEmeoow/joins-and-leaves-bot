e4cb64effc2077cf72f1bce8353d8dc3f17900a7
Meoow <95740100+AEmeoow@users.noreply.github.com>
2022-02-25 13:47:30 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+(async () => {
+    const Discord = require("discord.js");
+    const Database = require("easy-json-database");
+    const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    const s4d = {
+        Discord,
+        database: new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/db.json`),
+        joiningMember: null,
+        reply: null,
+        tokenInvalid: false,
+        tokenError: null,
+        checkMessageExists() {
+            if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+            if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+        }
+    };
+    s4d.client = new s4d.Discord.Client({
+        intents: [Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)],
+        partials: ["REACTION"]
+    });
+
+    await s4d.client.login('Your bot token').catch((e) => {
+        s4d.tokenInvalid = true;
+        s4d.tokenError = e;
+    });
+
+    s4d.client.on('guildMemberAdd', async (param1) => {
+        s4d.joiningMember = param1;
+        s4d.client.channels.cache.find((channel) => channel.name === 'joins-and-leaves').send(String((s4d.joiningMember.user.username)));
+        s4d.client.channels.cache.find((channel) => channel.name === 'joins-and-leaves').send(String('A new member just joined! They are member number'));
+        s4d.client.channels.cache.find((channel) => channel.name === 'joins-and-leaves').send(String(((s4d.client.guilds.cache.find((guild) => guild.name === 'Server Name Here')).memberCount)));
+        s4d.joiningMember = null
+    });
+
+    s4d.client.on('guildMemberRemove', async (param1) => {
+        s4d.leavingMember = param1;
+        s4d.client.channels.cache.find((channel) => channel.name === 'joins-and-leaves').send(String((s4d.leavingMember.user.username)));
+        s4d.client.channels.cache.find((channel) => channel.name === 'joins-and-leaves').send(String('Sadly someone just left. We are now at:'));
+        s4d.client.channels.cache.find((channel) => channel.name === 'joins-and-leaves').send(String(((s4d.client.guilds.cache.find((guild) => guild.name === 'Server Name Here')).memberCount)));
+        s4d.leavingMember = null
+    });
+
+
+    return s4d;
+})();
